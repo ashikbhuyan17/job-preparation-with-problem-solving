d8c631f9c736f33910e0b2aebdbafbb12bc10c4c
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-08-01 20:06:01 +0600

    prototype filter => even

---
+++ object-problem-solve/array-prototype/filter/even.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4];
+
+let data = numbers.filter(value => value % 2 == 0)
+console.log("even data", data);
---
+++ object-problem-solve/array-prototype/filter/filter.js
@@ -0,0 +1,8 @@
+/*
+syntax :
+var new_array = arr.filter(function callback(element, index, array) {
+    // Return true or false
+}[, thisArg])
+*/
+
+
