53475ff5c26c624ff12bc9045d80849e1fdf0bb3
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-08-01 20:08:42 +0600

    prototype filter => students grade

---
+++ object-problem-solve/array-prototype/filter/grade.js
@@ -0,0 +1,10 @@
+const students = [
+    { name: 'Quincy', grade: 96 },
+    { name: 'Jason', grade: 84 },
+    { name: 'Alexis', grade: 100 },
+    { name: 'Sam', grade: 65 },
+    { name: 'Katie', grade: 90 }
+];
+
+const student = students.filter(data => data.grade > 85)
+console.log("grade is gater than 80", student);
