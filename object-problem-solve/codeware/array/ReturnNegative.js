432a47be4af5bec495d14a77f8cdad5453dded4d
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-08-03 14:26:08 +0600

    codeware problem solving => return negative number

---
+++ object-problem-solve/codeware/array/ReturnNegative.js
@@ -0,0 +1,9 @@
+// function makeNegative(num) {
+//     return num > 0 ? (-num) : num
+// }
+
+// The Math.abs() function returns the absolute value of a number.
+function makeNegative(num) {
+    return -Math.abs(num);
+}
+console.log(makeNegative(-8));
