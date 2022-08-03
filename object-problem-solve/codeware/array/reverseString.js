526995e12760b4809f38e480c5d933bc76307114
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-08-03 15:04:23 +0600

    codeware problem solving => return reverse string

---
+++ object-problem-solve/codeware/array/reverseString.js
@@ -0,0 +1,33 @@
+// function reverseString(str) {
+//     // Step 1. Use the split() method to return a new array
+//     var splitString = str.split(""); // var splitString = "hello".split("");
+//     // ["h", "e", "l", "l", "o"]
+
+//     // Step 2. Use the reverse() method to reverse the new created array
+//     var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+//     // ["o", "l", "l", "e", "h"]
+
+//     // Step 3. Use the join() method to join all elements of the array into a string
+//     var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+//     // "olleh"
+
+//     //Step 4. Return the reversed string
+//     return joinArray; // "olleh"
+// }
+
+
+// const reverseString = str => str.split('').reverse().join('');
+const reverseString = str => [...str].reverse().join('');
+
+
+// function reverseString(s) {
+//     var o = '';
+//     for (var i = s.length - 1; i >= 0; i--)
+//         o += s[i];
+//     return o;
+// }
+
+
+
+
+console.log(reverseString("hello"))
